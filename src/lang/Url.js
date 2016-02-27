743beef9cac0b85525aabcef153c036c6c494830
afei <88603982@qq.com>
2016-02-27 22:52:53 +0800

    stay pure

--- src/lang/Url.js
+++
@@ -1,4 +0,0 @@
-(function (nx, global) {
-
-
-}(nx, nx.global));
--- test/lang/Date.js
+++
@@ -1,7 +0,0 @@
-module("Date.js");
-
-var DateUtil=nx.lang.Date;
-test("nx.lang.Date--repeat", function () {
-    ok(1===1,'124');
-});
-
